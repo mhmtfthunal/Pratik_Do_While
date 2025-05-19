e17f6c49a17f66ca29ff80c0f7c458fdc18f22f5
FatihNitro <FatihNitro@DESKTOP-KERR77G>
2025-05-19 09:44:37 +0300

    Add project files.

---
+++ Pratik_Do_While/Program.cs
@@ -0,0 +1,32 @@
+﻿//While İle Yapılan Örnek
+
+//Burada koşul en balta kontrol edildiğinden kullanıcı -5 girerse koşula uymadığından çalışmayacaktır.
+
+int sayac = 0;
+
+Console.Write("Lütfen bir sayı giriniz: ");
+int limit = Convert.ToInt32(Console.ReadLine());
+
+while (sayac <= limit)
+{
+    Console.WriteLine("Ben bir Patika'lıyım.");
+    sayac++;
+}
+
+Console.WriteLine("--------------------------------");
+
+//Do-While İle Yapılan Örnek
+
+//Burada koşul en sonda olduğu için kullanıcı -5 girse bile en az 1 kez çalıştırılır.
+
+int i = 0;
+
+Console.Write("Lütfen bir sayı giriniz: ");
+int sinir = Convert.ToInt32(Console.ReadLine());
+
+do
+{
+    Console.WriteLine("Ben bir Patika'lıyım.");
+    i++;
+
+} while (i <= sinir);
